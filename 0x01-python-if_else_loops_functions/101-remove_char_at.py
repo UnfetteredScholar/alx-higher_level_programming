6b5fee7e94a8b366b10d0a8e6623490df6d49931
Ato Toffah <atotoffah@gmail.com>
2023-08-10 01:58:18 +0000

    Added extra

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    if n < 0 or n >= len(str):
+        return str
+    if n == 0:
+        return str[1:]
+    else:
+        return str[0:n] + str[n + 1:]
