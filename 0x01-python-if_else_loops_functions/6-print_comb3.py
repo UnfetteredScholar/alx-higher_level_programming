ea02800b2f55e0edd558395dae341801585cb0ec
Ato Toffah <atotoffah@gmail.com>
2023-08-10 00:22:15 +0000

    Added

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for a in range(0, 9):
+    for b in range (a + 1, 10):
+        print(f"{a}{b}", end='')
+        if a == 8 and b == 9:
+            print()
+        else:
+            print(", ", end='')
+
