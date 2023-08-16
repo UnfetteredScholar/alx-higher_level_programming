0471fe3553e8675e8aacf09d014593e6c4363b1d
Ato Toffah <atotoffah@gmail.com>
2023-08-16 23:00:53 +0000

    Task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+            '_': 0  # Used to denote end of string
+            }
+    res = 0
+    if roman_string is None or roman_string == "":
+        return 0
+
+    roman = roman_string + '_'
+    for i in range(0, len(roman_string)):
+        if values[roman[i]] < values[roman[i + 1]]:
+            res -= values[roman[i]]
+        else:
+            res += values[roman[i]]
+    return res
