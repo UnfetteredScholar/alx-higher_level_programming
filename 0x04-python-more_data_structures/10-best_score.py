db0c21f69426f08e5c599b8225cba61f98443a5a
Ato Toffah <atotoffah@gmail.com>
2023-08-16 17:32:43 +0000

    Task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None or a_dictionary == {}:
+        return None
+    mkey = a_dictionary.keys()[0]
+
+    for key in a_dictionary.keys():
+        if a_dictionary[mkey] < a_dictionary[key]:
+            mkey = key
+    return mkey
