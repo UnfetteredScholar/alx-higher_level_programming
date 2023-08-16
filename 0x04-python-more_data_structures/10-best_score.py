21f776338f806f722763d134fe868433b82196bd
Ato Toffah <atotoffah@gmail.com>
2023-08-16 17:38:14 +0000

    Fix 1

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None or a_dictionary == {}:
         return None
-    mkey = a_dictionary.keys()[0]
+    mkey = list(a_dictionary.keys())[0]
 
     for key in a_dictionary.keys():
         if a_dictionary[mkey] < a_dictionary[key]:
