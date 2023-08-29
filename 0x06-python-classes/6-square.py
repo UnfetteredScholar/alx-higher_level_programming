eed9e3e5569c09e30ee24d0197a9e0421c24a4dc
Ato Toffah <atotoffah@gmail.com>
2023-08-29 22:43:22 +0000

    Fix 1

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -93,6 +93,8 @@ class Square:
         if self.__size == 0:
             print()
             return
+        for y in range(self.__position[1]):
+            print()
         for i in range(self.__size):
             line = ' ' * self.__position[0] + '#'*self.__size
             print(line)
