54fead6c59ca88a4b79d3126b16bdb6f2a0aa049
Ato Toffah <atotoffah@gmail.com>
2023-08-29 21:28:29 +0000

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a Class Square"""
+
+
+class Square:
+    """Square class"""
+
+    def __init__(self, size):
+        """Initializes a Square object
+            Args:
+                size (int): the size of the square
+        """
+        self.__size = size
