4139bd5cce5921f9b9807915fc3d28a83b797952
Ato Toffah <atotoffah@gmail.com>
2023-09-11 20:34:25 +0000

    Task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines the BaseGeomety class"""
+
+
+class BaseGeometry:
+    """The BaseGeometry class definition"""
+
+    def area(self):
+        """Raises exception not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates if the variable value is an integer
+        and is greater than 0"""
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        else if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
