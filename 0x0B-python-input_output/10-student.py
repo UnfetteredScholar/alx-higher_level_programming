3df2dc9f00e131b48c4a01a5201d3c4267545196
Ato Toffah <atotoffah@gmail.com>
2023-09-12 21:10:21 +0000

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Defines the Student class"""
+
+
+class Student:
+    """The Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Student class constructor"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Converts the class to a dictionary
+
+        Args:
+            attrs List[str]: list of attributes to return in dict
+
+        Returns:
+            Dictionary representation of object with specified attributes
+        """
+        if hasattr(self, "__dict__"):
+            jdict = self.__dict__.copy()
+            if attrs is not None:
+                keys = []
+                for key in jdict.keys():
+                    keys.append(key)
+                for key in keys:
+                    if key not in attrs:
+                        del jdict[key]
+            return jdict
+        else:
+            return {}
