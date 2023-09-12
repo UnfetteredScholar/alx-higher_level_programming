0217cc52cc99941f6537d6839d4d88951f4c12ab
Ato Toffah <atotoffah@gmail.com>
2023-09-12 20:29:39 +0000

    Fix 1

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 """Adds all command line arguments to a json file"""
 import sys
-
+import os.path
 
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 filename = "add_item.json"
-save_to_json_file(sys.argv[1:], filename)
+if os.path.isfile(filename):
+    items = load_from_json_file(filename)
+else:
+    items = []
+
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
