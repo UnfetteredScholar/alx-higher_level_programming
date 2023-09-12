cd0cf5c84026b01e51de4a6c056529a6be89605f
Ato Toffah <atotoffah@gmail.com>
2023-09-12 19:48:42 +0000

    Task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines the to_json_string function"""
+import json
+
+
+def to_json_string(my_obj):
+    """Converts an object to a json string"""
+
+    return json.dumps(my_obj)
