f60a7bb2d53d59366c06ff7b6d8a9e4594097b48
Ato Toffah <atotoffah@gmail.com>
2023-11-14 17:04:02 +0000

    Task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+// Defines a Rectangle class with width and height attributes.
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let line = '';
+    for (let i = 0; i < this.width; i++) {
+      line += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(line);
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
