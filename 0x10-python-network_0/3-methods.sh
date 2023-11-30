5f84126ab5e8ea05cee2a72b0f123c1763f4772a
Ato Toffah <atotoffah@gmail.com>
2023-11-30 20:22:38 +0000

    Task 3:Fix 1

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays all methods a server will accept for a URL
-curl -sI -X "$1" | grep -i "allow:" | sed 's/Allow: //'
+curl -sI -X "$1" | grep "Allow:" | sed -ne 's/^Allow: //p'
