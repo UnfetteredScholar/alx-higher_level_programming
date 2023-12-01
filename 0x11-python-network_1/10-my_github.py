d4ac6a40fa8c83f479e37364c16bc3dc3aebb940
Ato Toffah <atotoffah@gmail.com>
2023-12-01 21:14:37 +0000

    Task 9:Fix 1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     from requests.auth import HTTPBasicAuth
     from sys import argv
 
-    endpoint = f"https://api.github.com/users/{argv[1]}"
+    endpoint = f"https://api.github.com/user"
     basic_auth = HTTPBasicAuth(argv[1], argv[2])
 
     response = requests.get(endpoint, auth=basic_auth).json()
