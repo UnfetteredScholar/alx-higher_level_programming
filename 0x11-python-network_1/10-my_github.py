3b2f5e82f9c2efca0bfc0b86a462c8ef94718806
Ato Toffah <atotoffah@gmail.com>
2023-12-01 21:11:12 +0000

    Task 9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+
+if __name__ == "__main__":
+    import requests
+    from requests.auth import HTTPBasicAuth
+    from sys import argv
+
+    endpoint = f"https://api.github.com/users/{argv[1]}"
+    basic_auth = HTTPBasicAuth(argv[1], argv[2])
+
+    response = requests.get(endpoint, auth=basic_auth).json()
+    print(response.get('id'))
